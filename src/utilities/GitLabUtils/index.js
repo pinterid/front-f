d1ff35e4179d73ef9e74b4364411009e2812f7e1
auernige <auernige@edu.htl-villach.at>
2019-10-09 20:50:41 +0200

    Implemented getOrganizations
    With this function all Organizatiions a user is member of can be collected and is stored in the JSON structure

--- src/utilities/GitLabUtils/index.js
+++ src/utilities/GitLabUtils/index.js
@@ -1,13 +1,23 @@
+// Import JSON structure
+import { structure, organizations, members} from "./objects";
+let data = structure;
+
+//get Json data file
 export const get = (server, username) => {
-  getContributions(parseJsonToDOM(fetchJson(server, username)));
+  data.platformUrl = server;
+  // getContributions(server, username);
+  return getOrganizations(server, username)
+  .then(res => {
+    data.organizations = res;
+    return data;
+  });
 };
 
-// Fetch Activities from GitLab in JSON format
-const fetchJson = (server, username) => {
-  const limit = "2147483647"
-  const proxy = "https://c-hive-proxy.herokuapp.com/"
+// Fetch JSON from url
+const fetchJson = (urlIn) => {
+  const proxy = "https://c-hive-proxy.herokuapp.com/";
   
-  const url = `${proxy}https://${server}/${username}?limit=${limit}`
+  const url = `${proxy}${urlIn}`;
   return fetch(url, {
     headers: {
       Accept: "application/json, text/plain, */*",
@@ -18,7 +28,72 @@ const fetchJson = (server, username) => {
   .then(res => {return res})
 }
 
-// Parse plain text to DOM Object
+// Fetch HTML from url
+const fetchHtml = (urlIn) => {
+  const proxy = "https://c-hive-proxy.herokuapp.com/";
+  
+  const url = `${proxy}${urlIn}`;
+  return fetch(url, {
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "X-Requested-With": "XMLHttpRequest"
+    }
+  })
+  .then(async res => await res.text())
+  .then(res => {return res})
+}
+
+// Get all Organizations a user is in
+const getOrganizations = (server, username) => {
+  const limit = "2147483647";
+
+  const url = `https://${server}/users/${username}/groups.json?limit=${limit}`;
+  return parseJsonToDOM(fetchJson(url))
+  .then(res => {
+    const raw = res.getElementsByClassName("group-row");
+    let temp = [];
+    Array.from(raw).forEach(org => {
+      let orgObj = JSON.parse(JSON.stringify(organizations));
+      if(org.innerHTML.includes("<img")){
+        const img = org.getElementsByClassName("avatar").item(0);
+        orgObj.avatarUrl = img.dataset.src;
+      }
+      const groupNameLink = org.getElementsByClassName("group-name").item(0);
+
+      orgObj.name = groupNameLink.innerHTML;
+      orgObj.orgUrl = groupNameLink.pathname;
+      getGroupMembers(orgObj.orgUrl, server)
+      .then(res => {
+        orgObj.members = res;
+      });
+      temp.push(orgObj);
+    });
+    return temp;
+  });
+}
+// Get all Group Members from Group
+const getGroupMembers = (name, server) => {
+  const url = `https://${server}/groups${name}/-/group_members`;
+  return parseTextToDOM(fetchHtml(url))
+  .then(res => {
+    const raw = res.getElementsByClassName("list-item-name");
+    let temp = [];
+    Array.from(raw).forEach(item => {
+      let membersObj = JSON.parse(JSON.stringify(members));
+      const img = item.getElementsByClassName("avatar").item(0);
+      const userLink = item.getElementsByClassName("member").item(0);
+      const userSpan = userLink.nextElementSibling
+      membersObj.avatarUrl = img.dataset.src;
+      membersObj.name = userLink.innerHTML;
+      membersObj.username = userSpan.innerHTML.substring(1);
+      membersObj.webUrl = userLink.pathname;
+      temp.push(membersObj);
+    });
+    return temp;
+  });
+}
+
+// Parse Json to DOM Object
 const parseJsonToDOM = (json) => {
   const parser = new DOMParser()
   const html = json
@@ -28,38 +103,12 @@ const parseJsonToDOM = (json) => {
   return html;
 }
 
-// Get all Contributions from DOM Object
-const getContributions = (html) => {
-  const contribs = html
+//Parse plain text to DOM Object
+const parseTextToDOM = (json) => {
+  const parser = new DOMParser()
+  const html = json
   .then(res => {
-    let temp = [];
-    temp.push(getCommits(res));
-    temp.push(getIssues(res));
+    return parser.parseFromString(res, "text/html");
   });
-  return contribs;
-}
-
-// Get all Commits from DOM Object
-const getCommits = (html) => {
-  const activities = html.getElementsByClassName("event-item");
-  let commits = [];
-  Array.from(activities).forEach(a => {
-    if(a.innerHTML.includes("pushed to branch")){
-      commits.push(a);
-    }
-  });
-  return commits;
-}
-
-// Get all Issues from DOM Object
-const getIssues = (html) => {
-  const activities = html.getElementsByClassName("event-item");
-  let issues = [];
-  Array.from(activities).forEach(a => {
-    if(a.innerHTML.includes("opened")){
-      issues.push(a);
-    }
-  });
-  console.log(issues);
-  return issues;
+  return html;
 }
---
+++ src/utilities/GitLabUtils/objects.js
@@ -0,0 +1,158 @@
+export const structure = {
+    "platformName": "GitLab",
+    "platformUrl": "",
+    "avatarUrl": "",
+    "websiteUrl": "",
+    "company": "",
+    "email": "",
+    "name": "",
+    "createdAt": "",
+    "location": "",
+    "status": {
+      "message": "",
+      "emojiHTML": ""
+    },
+    "organizations": [],
+    "repos": [],
+    "contributions": {
+      "years": []
+    }
+}
+
+export const organizations = {
+    "avatarUrl": "",
+    "name": "",
+    "orgUrl": "",
+    "members": []
+}
+
+export const repos = {
+    "avatarUrl": "",
+    "name": "",
+    "repoUrl": "",
+    "owner": {
+      "avatarUrl": "",
+      "name": "",
+      "username": "",
+      "webUrl": ""
+    },
+    "members": [],
+    "languagesPie": {
+      "size": "",
+      "count": "",
+      "edges": [],
+      "slice": {
+        "size": "",
+        "count": "",
+        "edges": []
+      }
+    }
+}
+
+export const members = {
+    "avatarUrl": "",
+    "name": "",
+    "username": "",
+    "webUrl": ""
+}
+
+export const edges = {
+    "name": "",
+    "size": "",
+    "color": ""
+}
+
+export const years = {
+    "stats": {
+      "stats": {
+        "commits": {
+          "total": "",
+          "reposCount": ""
+        },
+        "issues": {
+          "total": "",
+          "reposCount": ""
+        },
+        "pullRequests": {
+          "total": "",
+          "reposCount": ""
+        },
+        "codeReviews": {
+          "total": "",
+          "reposCount": ""
+        },
+        "busiestDay": "",
+        "average": "",
+        "streak": {
+          "currentStreak": {
+            "startDate": "",
+            "endDate": "",
+            "total": ""
+          },
+          "longestStreak": {
+            "startDate": "",
+            "endDate": "",
+            "total": ""
+          },
+          "history": []
+        }
+      }
+    },
+    "weeks": []
+}
+
+export const history = {
+    "startDate": "",
+    "endDate": "",
+    "total": ""
+}
+
+export const weeks = {
+    "days": []
+}
+
+export const days =  {
+    "date": "",
+    "total": "",
+    "contributions:": {
+      "commits": [],
+      "codeReviews": [],
+      "pullRequests": [],
+      "issues": []
+    }
+}
+
+export const commits = {
+    "organisationName": "",
+    "repoUrl": "",
+    "commitUrl": "",
+    "languages": {
+      "size": "",
+      "count": "",
+      "edges": []
+    }
+}
+
+export const codeReviews = {
+    "filesCount": "",
+    "repoUrl": "",
+    "discussionCount": "",
+    "codeReviewUrl": "?"
+}
+
+export const pullRequests = {
+    "discussionCount": "",
+    "repoUrl": "",
+    "commitCount": ""
+}
+
+export const issues = {
+    "conversationCount": "",
+    "labels": []
+}
+
+export const labels = {
+    "name": "",
+    "description": "",
+    "color": ""
+}
