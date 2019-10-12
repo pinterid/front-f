ac6218bc627cf258d427fded7a2da7d3e79e7300
Pinterid <david.pinterics@gmail.com>
2019-10-12 15:59:06 +0200

    Fix Redirect
    The redirect page componennt is now as requested

--- src/components/pages/OAuthRedirect/index.jsx
+++ src/components/pages/OAuthRedirect/index.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { MDBContainer} from "mdbreact";
-class Redirect extends React.Component
-{
-    getRedirect = async () => {
-        // Get name of window which was set by the parent to be the unique request key
-        const requestKey = window.name;
-        // Update corresponding entry with the redirected url which should contain either access token or failure reason in the query parameter / hash
-        window.localStorage.setItem(requestKey, window.location.href);
-        window.close();
-    }
-    render () {
-        return (
-            <MDBContainer>
-                <html onLoad={this.getRedirect()}>
-                    <head>
-                        <title>React Simple Auth - Redirect</title>
-                    </head>
-                    <body>
-                        <h1>React Simple Auth</h1>
-                    </body>
-                </html>
-            </MDBContainer>
-        );
-    }
+//> React
+// Contains all the functionality necessary to define React components
+import React from 'react';
+
+class Redirect extends React.Component {
+
+  componentDidMount = () => {
+    this.getRedirect();
+  }
+
+  getRedirect = async () => {
+      // Get name of window which was set by the parent to be the unique request key
+      const requestKey = window.name;
+      // Update corresponding entry with the redirected url which should contain either access token or failure reason in the query parameter / hash
+      localStorage.setItem(requestKey, window.location.href);
+      window.close();
+  }
+
+  render () {
+    return (
+      null
+    );
+  }
 }
 export default Redirect;
+
