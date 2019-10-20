78a0c861aaeff474cfd2c125ef9fa328365f282c
Christian Aichner <contact@aichner-christian.com>
2019-10-20 16:41:36 +0200

    Add dynamic contrib chart
    The contribution chart is now being filled with dummy
    data. It now supports dynamic generation.

--- src/components/molecules/ContribChart/index.jsx
+++ src/components/molecules/ContribChart/index.jsx
@@ -7,33 +7,40 @@ import {
   MDBContainer
 } from "mdbreact";
 
+//> Dummy data
+const data = [
+  {
+    source: "GitHub",
+    background: "rgba(51,51,51,0)",
+    border: "rgba(51,51,51,.4)",
+    data: [10, 15, 15, 70]
+  },
+  {
+    source: "GitLab",
+    background: "rgba(226,67,41,0)",
+    border: "rgba(226,67,41,.4)",
+    data: [17, 30, 25, 44]
+  }
+];
 
 class ChartsPage extends React.Component {
-  state = {
+
+  state={
     dataRadar: {
-      labels: ["Code review", "Issues", "Pull request", "Commits"],
-      datasets: [
-        {
-          label: "GitHub",
-          backgroundColor: "rgba(51, 51, 51,0)",
-          borderColor: "rgba(51, 51, 51,.4)",
-          data: [10, 15, 15, 70]
-        },
-        {
-          label: "GitLab",
-          backgroundColor: "rgba(255,255,255,0)",
-          borderColor: "rgba(266,67,41,.4)",
-          data: [17, 30, 25, 44]
-        },
-        {
-          label: "Bitbucket",
-          backgroundColor: "rgba(255,255,255,0)",
-          borderColor: "rgba(32, 80, 129,.4)",
-          data: [5, 2, 5, 62]
-        }
-      ]
-    },
-    dataRadarOptions: {
+      labels: ["Loading"],
+      datasets: [{
+        label: "Loading"
+      }]
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      dataRadar: {
+        labels: ["Code review", "Issues", "Pull request", "Commits"],
+        datasets: this.calculateSources()
+      },
+      dataRadarOptions: {
         responsive: true,
         elements: {
           line: {
@@ -73,10 +80,54 @@ class ChartsPage extends React.Component {
             }]
         }
     }
+    })
   }
 
+  calculateSources = () => {
+    let totalReviews = 0, totalIssues = 0, totalRequests = 0, totalCommits = 0, totalSources = 1;
 
-render() {
+    // Get data points for all sources
+    let results = data.map((source, i) => {
+      totalReviews += source.data[0];
+      totalIssues += source.data[1];
+      totalRequests += source.data[2];
+      totalCommits += source.data[3];
+
+      totalSources += i;
+
+      return {
+          label: source.source,
+          backgroundColor: source.background,
+          borderColor: source.border,
+          data: source.data
+      }
+    });
+
+    // Calculate averages
+    let avgReviews, avgIssues, avgRequests, avgCommits;
+
+    avgReviews = parseInt(totalReviews) / parseInt(totalSources);
+    avgIssues = parseInt(totalIssues) / parseInt(totalSources);
+    avgRequests = parseInt(totalRequests) / parseInt(totalSources);
+    avgCommits = parseInt(totalCommits) / parseInt(totalSources);
+
+    // Create average data points
+    results.push({
+      label: "Average",
+      backgroundColor: "rgba(33, 181, 33, .7)",
+      borderColor: "rgba(33, 181, 33, .3)",
+      data: [
+        avgReviews,
+        avgIssues,
+        avgReviews,
+        avgCommits
+      ]
+    });
+
+    return results;
+  }
+
+  render() {
     return (
       <MDBContainer>
         <Radar 
