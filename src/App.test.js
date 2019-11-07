91c9035f2bfc13669a6e0f4ca2517d368a26e036
Elias Auernig <auernige@edu.htl-villach.at>
2019-11-07 13:22:59 +0100

    Implement Base for Utils
    Program is now able to start without errors. Implemented base to write GitLab and GitHub utils

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,3 @@
-import * as gitLab from './utilities/GitLabUtils';
-
-/*> GitLab Util
- * The point of this utility is to get specific data
- * of a specific user.
- *
- * Usage:
- * gitLab
- * .get('<GitLab server>', '<username>')
- * .then(res => console.log(res));
- */
-
 import * as intel from './intel'
 
 /*> Intel
--- src/intel/index.js
+++ src/intel/index.js
@@ -1,15 +1,12 @@
 import * as statements from './sql/statements';
 import * as alasql from 'alasql'
 
-import { Database } from "./dummy";
-//import { Database } from "./implement";
+//import { Database } from "./dummy";
+import { Database } from "./implement";
 
 Database();
 
 export const testCall = () => {
   //console.log(db.exec('SELECT * FROM organization_has_member'))
-  console.log(db.exec('SELECT * FROM statistic'))
-  console.log(db.exec('SELECT * FROM statistic WHERE id=1'))
-  db.exec('UPDATE statistic SET current_streak_id = ? WHERE id = ?', [1,1])
-  console.log(db.exec('SELECT * FROM statistic'))
+  console.log(alasql('SELECT * FROM platform'));
 }
